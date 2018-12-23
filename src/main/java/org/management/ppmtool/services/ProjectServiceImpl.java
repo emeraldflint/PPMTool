0506add8ede53b7390219537ce7fbc91fe3bfb3c
Eugene Kasap <ekasap@basketsavings.com>
2018-12-24 00:42:32 +0200

    Added error service.

--- src/main/java/org/management/ppmtool/services/ProjectServiceImpl.java
+++ src/main/java/org/management/ppmtool/services/ProjectServiceImpl.java
@@ -2,7 +2,6 @@ package org.management.ppmtool.services;
 
 import org.management.ppmtool.domain.Project;
 import org.management.ppmtool.repositories.ProjectRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +9,6 @@ public class ProjectServiceImpl implements ProjectService{
 
     private ProjectRepository projectRepository;
 
-    @Autowired
     ProjectServiceImpl(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
     }
--- src/main/java/org/management/ppmtool/web/ProjectController.java
+++ src/main/java/org/management/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package org.management.ppmtool.web;
 
 import org.management.ppmtool.domain.Project;
+import org.management.ppmtool.services.MapValidationErrorService;
 import org.management.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,28 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     private ProjectService projectService;
+    private MapValidationErrorService mapValidationErrorService;
 
-    ProjectController(ProjectService projectService) {
+    ProjectController(ProjectService projectService, MapValidationErrorService mapValidationErrorService) {
         this.projectService = projectService;
+        this.mapValidationErrorService = mapValidationErrorService;
     }
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-
-            result.getFieldErrors().forEach(fieldError -> errorMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
-
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
