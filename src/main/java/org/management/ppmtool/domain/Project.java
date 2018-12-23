9e67e61499febdc67d3d338dd9ceafe1fa111b60
Eugene Kasap <ekasap@basketsavings.com>
2018-12-23 17:00:28 +0200

    Added project projectlombok dependency Updated spring boot version to 2.1.1 Added Project entity and ProjectRepository.

---
+++ src/main/java/org/management/ppmtool/domain/Project.java
@@ -0,0 +1,43 @@
+package org.management.ppmtool.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+
+}
