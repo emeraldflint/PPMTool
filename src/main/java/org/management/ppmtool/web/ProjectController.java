872a6b5769b69a60ee9f5a852a561b130ae92617
ekasap <skiper.emerald@gmail.com>
2018-12-23 21:44:30 +0200

    Added "dummy" validation to ProjectController.

--- src/main/java/org/management/ppmtool/web/ProjectController.java
+++ src/main/java/org/management/ppmtool/web/ProjectController.java
@@ -4,10 +4,13 @@ import org.management.ppmtool.domain.Project;
 import org.management.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,7 +23,11 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
