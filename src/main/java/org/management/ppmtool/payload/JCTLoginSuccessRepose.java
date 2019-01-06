a63c1cf0760d50a120ae53faf5dcf8d3ed05044d
Eugene Kasap <ekasap@basketsavings.com>
2019-01-07 00:50:47 +0200

    Added authenticateUser() api call.

--- src/main/java/org/management/ppmtool/payload/JCTLoginSuccessRepose.java
+++ src/main/java/org/management/ppmtool/payload/JCTLoginSuccessRepose.java
@@ -1,13 +1,15 @@
 package org.management.ppmtool.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @ToString
-public class JWTLoginSucessReponse {
+public class JCTLoginSuccessRepose {
     private boolean success;
     private String token;
 }
--- src/main/java/org/management/ppmtool/web/UserController.java
+++ src/main/java/org/management/ppmtool/web/UserController.java
@@ -1,10 +1,17 @@
 package org.management.ppmtool.web;
 
 import org.management.ppmtool.domain.User;
+import org.management.ppmtool.payload.JCTLoginSuccessRepose;
+import org.management.ppmtool.payload.LoginRequest;
+import org.management.ppmtool.security.JwtTokenProvider;
 import org.management.ppmtool.services.MapValidationErrorService;
 import org.management.ppmtool.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +20,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static org.management.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private MapValidationErrorService mapValidationErrorService;
     private UserService userService;
+    private JwtTokenProvider tokenProvider;
+    private AuthenticationManager authenticationManager;
 
-    public UserController(MapValidationErrorService mapValidationErrorService, UserService userService) {
+    public UserController(MapValidationErrorService mapValidationErrorService, UserService userService, JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager) {
         this.mapValidationErrorService = mapValidationErrorService;
         this.userService = userService;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+
+
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JCTLoginSuccessRepose(true, jwt));
     }
 
     @PostMapping("/register")
@@ -34,6 +66,6 @@ public class UserController {
 
         User newUser = userService.saveUser(user);
 
-        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 }
