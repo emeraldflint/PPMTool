ec5fb3d417b2f292170490e6cec865b57a390bac
Eugene Kasap <ekasap@basketsavings.com>
2019-01-07 02:04:22 +0200

    Added custom JWT filter to use in tokens.

--- src/main/java/org/management/ppmtool/security/JwtTokenProvider.java
+++ src/main/java/org/management/ppmtool/security/JwtTokenProvider.java
@@ -1,8 +1,13 @@
 package org.management.ppmtool.security;
 
-import org.management.ppmtool.domain.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.management.ppmtool.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +22,6 @@ import static org.management.ppmtool.security.SecurityConstants.SECRET;
 public class JwtTokenProvider {
 
     //Generate the token
-
     public String generateToken(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         Date now = new Date(System.currentTimeMillis());
@@ -38,5 +42,35 @@ public class JwtTokenProvider {
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, SECRET)
                 .compact();
+    }
+
+    //Validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+
+        return false;
+    }
+
+    //Get user Id from token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+
+        String id = (String) claims.get("id");
+
+        return Long.parseLong(id);
     }
 }
