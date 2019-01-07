4ea45dd8bad26dc0f612ccbe6f52eb19dca0f0fe
Eugene Kasap <ekasap@basketsavings.com>
2019-01-07 21:13:28 +0200

    Added lock operation for specific user for adding new task.

--- src/main/java/org/management/ppmtool/services/ProjectTaskService.java
+++ src/main/java/org/management/ppmtool/services/ProjectTaskService.java
@@ -3,9 +3,9 @@ package org.management.ppmtool.services;
 import org.management.ppmtool.domain.ProjectTask;
 
 public interface ProjectTaskService {
-    ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask);
+    ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask, String username);
 
-    Iterable<ProjectTask> findBacklogById(String id);
+    Iterable<ProjectTask> findBacklogById(String id, String username);
 
     ProjectTask findPTByProjectSequence(String backlog_id, String pt_id);
 
--- src/main/java/org/management/ppmtool/services/ProjectTaskServiceImpl.java
+++ src/main/java/org/management/ppmtool/services/ProjectTaskServiceImpl.java
@@ -15,55 +15,49 @@ public class ProjectTaskServiceImpl implements ProjectTaskService {
     private BacklogRepository backlogRepository;
     private ProjectTaskRepository projectTaskRepository;
     private ProjectRepository projectRepository;
+    private ProjectService projectService;
 
-    public ProjectTaskServiceImpl(BacklogRepository backlogRepository, ProjectTaskRepository projectTaskRepository, ProjectRepository projectRepository) {
+    public ProjectTaskServiceImpl(BacklogRepository backlogRepository, ProjectTaskRepository projectTaskRepository, ProjectRepository projectRepository, ProjectService projectService) {
         this.backlogRepository = backlogRepository;
         this.projectTaskRepository = projectTaskRepository;
         this.projectRepository = projectRepository;
+        this.projectService = projectService;
     }
 
     @Override
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        try {
-            //PTs to be added to a specific project, project != null, BL exists
-            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-            //set the bl to pt
-            projectTask.setBacklog(backlog);
-            //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
-            Integer BacklogSequence = backlog.getPTSequence();
-            // Update the BL SEQUENCE
-            BacklogSequence++;
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask, String username) {
+        //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = projectService.findProjectByIdentifier(projectIdentifier, username).getBacklog();
+        //set the bl to pt
+        projectTask.setBacklog(backlog);
+        //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        // Update the BL SEQUENCE
+        BacklogSequence++;
 
-            backlog.setPTSequence(BacklogSequence);
+        backlog.setPTSequence(BacklogSequence);
 
-            //Add Sequence to Project Task
-            projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
-            projectTask.setProjectIdentifier(projectIdentifier);
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
-            //INITIAL priority when priority null
+        //INITIAL priority when priority null
 
-            //INITIAL status when status is null
-            if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
-                projectTask.setStatus("TO_DO");
-            }
-
-            if (projectTask.getPriority() == null) { //In the future we need projectTask.getPriority()== 0 to handle the form
-                projectTask.setPriority(3);
-            }
-
-            return projectTaskRepository.save(projectTask);
-        } catch (Exception e) {
-            throw new ProjectNotFoundException("Project not Found");
+        //INITIAL status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
         }
+
+        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+            projectTask.setPriority(3);
+        }
+
+        return projectTaskRepository.save(projectTask);
     }
 
     @Override
-    public Iterable<ProjectTask> findBacklogById(String id) {
-        Project project = projectRepository.findByProjectIdentifier(id);
-
-        if (project == null) {
-            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
-        }
+    public Iterable<ProjectTask> findBacklogById(String id, String username) {
+        projectService.findProjectByIdentifier(id, username);
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
